1768a7f79dc7feaccd9cf99a54b72d67db1740ac
Magnus West Madsen <cph-mm454@cphbusiness.dk>
2019-08-20 11:18:25 +0200

    first push

---
+++ Assets/Helloworld.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Helloworld : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        print("Hello World");
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(Input.GetKeyDown(KeyCode.F))
+        {
+            print("Respect");
+        }
+    }
+
+    public void Botton1()
+    {
+        print("i have been clicked");
+    }
+
+    public GameObject shootingStar;
+    
+    public void Spawn()
+    { 
+        Instantiate(shootingStar);
+    }
+}
---
+++ Assets/Movement.cs
@@ -0,0 +1,45 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Movement : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+
+    }
+
+
+    
+
+    // Update is called once per frame
+    void Update()
+    {
+
+        if (Input.GetKey(KeyCode.A)) 
+        {
+            transform.Translate(-7f * Time.deltaTime, 0f, 0f);
+        }
+        if (Input.GetKey(KeyCode.D))
+        {
+            transform.Translate(7f * Time.deltaTime, 0f, 0f);
+        }
+        if (Input.GetKey(KeyCode.W))
+        {
+            transform.Translate(0f, 0f, 7f * Time.deltaTime);
+        }
+        if (Input.GetKey(KeyCode.S))
+        {
+            transform.Translate(0f, 0f, -7f * Time.deltaTime);
+        }
+        if (Input.GetKey(KeyCode.Space))
+        {
+            transform.Translate(0f, 7f * Time.deltaTime, 0f);
+        }
+        if (Input.GetKey(KeyCode.LeftControl))
+        {
+            transform.Translate(0f, -7f * Time.deltaTime, 0f);
+        }
+    }
+}
